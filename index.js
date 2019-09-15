2510827070935c9f4ac62ad8658e58c769246c21
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2019-09-15 21:42:00 +0800

    asynchronous

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Before');
+const user = getUser(1);
+console.log(user)
+console.log('After');
+
+function getUser(id) {setTimeout(()=>{
+	console.log('Reading a user from a database......')
+	return{id: id, getGubusername:'Simon'}
+},2000);
+return 1;
+}
