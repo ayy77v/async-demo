09099b8de71579253f16049571c2330698df51eb
王資猛 <wangzimeng@wangzimengde-MacBook-Pro.local>
2019-09-15 22:29:26 +0800

    callback

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user)
+getUser(1, function(user){
+	console.log('User',user);
+	getRepositories(user.gitHubUsername,(repos)=>{
+		console.log('Repos', repos)
+	})
+});
+
 console.log('After');
 
-function getUser(id) {setTimeout(()=>{
+function getUser(id,callback) {
+	setTimeout(()=>{
 	console.log('Reading a user from a database......')
-	return{id: id, getGubusername:'Simon'}
+	callback({id: id, gitHubUsername:'Simon'});
 },2000);
-return 1;
+}
+
+function getRepositories(username, callback){
+	setTimeout(()=>{
+    console.log(username +' github repos')
+	callback( ['repo1', 'repo2', 'repo3']);
+},2000);
 }
